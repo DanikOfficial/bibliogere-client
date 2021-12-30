5edc9b87fd88fed4c80436384bf288af90503ef7
Pete J. Chiboleca <pete9450@gmail.com>
2021-12-31 01:16:54 +0200

    Application base slice

---
+++ src/features/api/apiSlice.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { RootState } from '../../app/store'
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api/v1',
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).user.token
+
+      if (token) headers.set('Authorization', `Bearer ${token}`)
+
+      return headers
+    },
+  }),
+  endpoints: (builder) => ({}),
+})
