a350595f02bc2b0a041b35eee374f6e1bb46c8b3
Pete J. Chiboleca <pete9450@gmail.com>
2022-01-19 15:15:41 +0200

    Refactored the code

--- src/utils/Routes.tsx
+++ src/utils/Routes.tsx
@@ -1,10 +1,6 @@
 import { useRoutes } from 'react-router'
 import homeRoutes from './routes/routes'
 
-const Routes: React.FC = () => {
-  const routes = useRoutes(homeRoutes)
-
-  return routes
-}
+const Routes: React.FC = () => useRoutes(homeRoutes)
 
 export default Routes
