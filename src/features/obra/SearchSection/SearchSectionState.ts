da83f0878d189742a03267d28187425586f8ba0d
Pete João Chiboleca <pete9450@gmail.com>
2022-02-25 17:37:37 +0200

    updated search component initial state

--- src/features/obra/SearchSection/SearchSectionState.ts
+++ src/features/obra/SearchSection/SearchSectionState.ts
@@ -1,6 +1,13 @@
+import { Option } from '../../../app/interfaces/Option'
 import { FormSearchState } from '../data/ObraInterfaces'
 
 export const InitialAdvancedSearchState: FormSearchState = {
   ano: '',
   autor: '',
 }
+
+export const tipoObraOptionsInitialState: Option[] = [
+  { value: '', label: 'Tipo de obra por pesquisar' },
+  { value: 'monografia', label: 'Monografia' },
+  { value: 'livro', label: 'Livro' },
+]
